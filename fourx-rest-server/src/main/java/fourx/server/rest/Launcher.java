420ecb261ebe94e53835ee1305663f97ac195c91
Jani Kaarela <jani.kaarela@saunalahti.com>
2014-01-13 15:24:28 +0200

    Tests for Launcher. Removed TODOs from web client pom.

--- fourx-rest-server/src/main/java/fourx/server/rest/Launcher.java
+++ fourx-rest-server/src/main/java/fourx/server/rest/Launcher.java
@@ -9,18 +9,30 @@ import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
+ * Launcher for running REST server in standalone mode.
  * 
  * @author Jani Kaarela (@gmail.com)
  */
 public class Launcher {
     
-    //TODO: configurable port etc...
+    public static final String ADDRESS_PROPERTY_KEY = "fourx.server.rest.baseAddress";
+    public static final String PORT_PROPERTY_KEY = "fourx.server.rest.port";
+    static final String DEFAULT_ADDRESS = "http://localhost/";
+    static final int DEFAULT_PORT = 8080;
     
     public static void main(String[] args) throws Exception {
-	URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080).build();
+	URI baseUri = UriBuilder.fromUri(determineBaseAddress()).port(determinePort()).build();
 	ResourceConfig config = new RestApplication();
 	Server server = JettyHttpContainerFactory.createServer(baseUri, config);
 	server.start();
+    }
+    
+    static String determineBaseAddress() {
+	return System.getProperty(ADDRESS_PROPERTY_KEY, DEFAULT_ADDRESS);
+    }
+    
+    static int determinePort() {
+	return Integer.parseInt(System.getProperty(PORT_PROPERTY_KEY, String.valueOf(DEFAULT_PORT)));
     }
     
 }
