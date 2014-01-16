1ba4eaad824fdf32c1288e26774d244694c5852a
Jani Kaarela <jani.kaarela@saunalahti.com>
2014-01-16 22:03:19 +0200

    Conversion to cartesian (x,y,z) coordinates.

--- fourx-core/src/main/java/fourx/utils/GalacticCoordsConverter.java
+++ fourx-core/src/main/java/fourx/utils/GalacticCoordsConverter.java
@@ -19,23 +19,38 @@ public class GalacticCoordsConverter {
 	 * 
 	 * @return	coordinates.
 	 */
-	public Coordinates convertToCoordinates(float longitude, float latitude, int distance) {
-		long z = Math.round(Math.sin(latitude) * distance); // TODO: what about negative latitude?
-		/*
-		long x...
-		long y...
-		TODO: how to handle negative longitude and longitude > 90 in the calculations?! THINK!
-		x is something to the effect of Math.sin(longitude) * planarDistance 
-		TODO: check values against Integer.MAX_VALUE and throw IllegalArgumentException, if exceeded
-		*/
-		return null;
-	}
+	public Coordinates convertToCartesian(float longitude, float latitude, int distance) {
+		long x = Math.round(distance * Math.sin(longitude) * Math.cos(latitude));
+		long y = Math.round(distance * Math.sin(longitude) * Math.sin(latitude));
+		long z = Math.round(distance * Math.cos(longitude));
+		if (x > Integer.MAX_VALUE || y > Integer.MAX_VALUE || z > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Distance " + distance + " too great: "
+					+ "resulting coordinates exceed maximum Integer value.");
+		}
+		return new Coordinates((int) x, (int) y, (int) z);
+	} // TODO: reverse conversion, tests
 	
+	/**
+	 * Reads in arguments as coordinates in the format: <code>longitude;latitude;distance</code>,
+	 * multiple coordinates as separate arguments. Longitude and latitude should be valid
+	 * <code>floats</code> while distance should be an integer (any unit will do, but 1/100
+	 * light years is the assumed unit).
+	 * 
+	 * @param args	command line arguments.
+	 */
 	public static void main(String[] args) {
 		if (args.length > 0) {
-			String[] data = args;
 			GalacticCoordsConverter converter = new GalacticCoordsConverter();
-			// TODO: read in data (format?), convert and print
+			for (String arg : args) {
+				String[] parts = arg.split(";");
+				if (parts.length != 3) {
+					throw new IllegalArgumentException("Invalid argument: " + arg);
+				}
+				float longitude = Float.parseFloat(parts[0]);
+				float latitude = Float.parseFloat(parts[1]);
+				int distance = Math.abs(Integer.parseInt(parts[2]));
+				System.out.println(converter.convertToCartesian(longitude, latitude, distance));
+			}
 		} else {
 			throw new IllegalArgumentException("No data to convert!");
 		}
