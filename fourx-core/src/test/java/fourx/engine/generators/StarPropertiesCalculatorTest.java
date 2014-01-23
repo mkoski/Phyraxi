a8d409e06eda5d5301d457ec6284cbf6a26df197
Jani Kaarela <jani.kaarela@saunalahti.com>
2014-01-23 22:07:06 +0200

    StarPropertiesCalculator and tests added.

---
+++ fourx-core/src/test/java/fourx/engine/generators/StarPropertiesCalculatorTest.java
@@ -0,0 +1,35 @@
+package fourx.engine.generators;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author Jani Kaarela (@gmail.com)
+ *
+ */
+public class StarPropertiesCalculatorTest {
+	
+	private final StarPropertiesCalculator calculator = new StarPropertiesCalculator();
+	
+	@Test
+	public void brightnessOfTheSunShouldBeOne() {
+		assertEquals(1.0, calculator.calculateBrightness(1.0), 0.01);
+	}
+	
+	@Test
+	public void brightnessOfAlphaCentauriShouldMatchObserved() {
+		assertEquals(1.519, calculator.calculateBrightness(1.1), 0.1);
+	}
+	
+	@Test
+	public void brightnessOfAEAurigaeShouldMatchObserved() {
+		assertEquals(33000, calculator.calculateBrightness(17), 3300);
+	}
+	
+	@Test
+	public void brightnessOfGliese687ShouldMatchObserved() {
+		assertEquals(0.00389, calculator.calculateBrightness(0.401), 0.1);
+	}
+
+}
