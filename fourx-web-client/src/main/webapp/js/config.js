2c7dbdf6679bcd0c0d91150087183fdf9e74a717
Markus Koski <mkoski@Markus-Koskis-iMac.local>
2014-01-29 18:54:36 +0200

    a bunch of js libs added

---
+++ fourx-web-client/src/main/webapp/js/config.js
@@ -0,0 +1,10 @@
+var starmap_url = "http://localhost:8080/resources/dummy-starmap";
+var spectral_types = {
+        O : 0x0000ff, // blue
+        B : 0x00ffff, // blue-white
+        A : 0xffffff, // white
+        F : 0xffff99, // yellow-whit
+        G : 0xffff00, // yellow
+        K : 0xff9933, // orange
+        M : 0xff0000 //red
+      };
