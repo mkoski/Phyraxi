5b8967539d019f03479ea3b956f78724654103be
Jani Kaarela <jani.kaarela@saunalahti.com>
2014-04-15 12:27:25 +0100

    Work in progress: determining tidal locking.

--- phyraxi-core/src/main/java/phyraxi/utils/PlanetPropertiesCalculator.java
+++ phyraxi-core/src/main/java/phyraxi/utils/PlanetPropertiesCalculator.java
@@ -1,9 +1,12 @@
 package phyraxi.utils;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import phyraxi.domain.Star;
 
 /**
- * Calculater for planet properties etc.
+ * Calculator for planet properties etc.
  * 
  * @author Jani Kaarela (@gmail.com)
  */
@@ -12,11 +15,13 @@ public class PlanetPropertiesCalculator {
 	static final double SUN_LUMINOSITY_IN_WATTS = 3.846e26;
 	private static final double STEFAN_BOLTZMANN_CONSTANT = 5.670373e-8;
 	private static final double ASTRONOMICAL_UNIT_IN_METERS = 1.496e11;
-	private static final double APPROXIMATE_PLANET_RIGIDITY = 2e10;
-	private static final double EART_RADIUS_IN_METERS = 6371000;
+	private static final int EARTH_RADIUS_IN_METERS = 6_371_000;
 	private static final double EARTH_MASS_IN_KILOGRAMS = 5.97219e24;
 	private static final double SUN_MASS_IN_KILOGRAMS = 1.9891e30;
-	private static final double CELESTIAL_TIME_SCALE = 0.2e10; // 2 billion years
+	private static final double GRAVITATIONAL_CONSTANT = 6.67384e-11;
+	private static final BigDecimal APPROXIMATE_PLANET_RIGIDITY = BigDecimal.valueOf(3e10);
+	private static final BigDecimal TIDAL_LOCKING_FORMULA_TIME_SCALE = BigDecimal.valueOf(1e10);
+	private static final BigDecimal CELESTIAL_TIME_SCALE = BigDecimal.valueOf(2_000_000_000);
 	
 	/**
 	 * Calculate planetary equilibrium temperate.
@@ -40,14 +45,15 @@ public class PlanetPropertiesCalculator {
 	double calculateFluxDensity(Star star, int distance) {
 		return (
 				convertSolarLuminositiesToWatts(star.getBrightness()) // radiation
-				/ (4 * Math.PI * Math.pow(convertAstronomicalUnitsToMeters(distance), 2))); // spherical area at distance
+				/ (4 * Math.PI * Math.pow(convertCentiAstronomicalUnitsToMeters(distance), 2))); // spherical area at distance
 	}
 	
 	double convertSolarLuminositiesToWatts(double luminosity) {
 		return luminosity * SUN_LUMINOSITY_IN_WATTS;
 	}
 	
-	double convertAstronomicalUnitsToMeters(int distance) {
+	// notice distance should be in 1/100 AUs
+	double convertCentiAstronomicalUnitsToMeters(int distance) {
 		return ASTRONOMICAL_UNIT_IN_METERS * (distance / 100d);
 	}
 	
@@ -63,40 +69,72 @@ public class PlanetPropertiesCalculator {
 	 * @return
 	 */
 	public double calculateHillSphereRadius(int distance, double starMass, double planetMass) {
-		return convertAstronomicalUnitsToMeters(distance) * (Math.pow((planetMass / starMass), (1/3)));
+		return convertCentiAstronomicalUnitsToMeters(distance) * (Math.pow((planetMass / starMass), (1/3)));
 	}
 	
 	/**
 	 * Calculates Roche Limit for a planet and a (rigid) satellite. A satellite orbiting at or under the limit
-	 * will be destroyed by tidal forces and turn into a ring of debris. 
+	 * will be destroyed by tidal forces and turn into a ring of debris. Density unit does not matter as long
+	 * as it is the same for both the planet and the satellite. 
 	 * 
 	 * @param planetDensity		planet density.
 	 * @param satelliteDensity	satellite density.
 	 * 
 	 * @return	Roche Limit, as multiples of planet radius.
 	 */
-	public double calculateRocheLimit(int planetDensity, int satelliteDensity) {
+	public double calculateRocheLimit(double planetDensity, double satelliteDensity) {
 		return 1.44 * (Math.pow((planetDensity / satelliteDensity), (1/3)));
 	}
 	
-	// TODO: method for determining tidal locking, see http://en.wikipedia.org/wiki/Tidal_locking
-	public boolean isPlanetTidallyLocked(int planetDistance, int planetRadius, int planetMass, double starMass) {
-		return (
-				6 * (Math.pow(convertAstronomicalUnitsToMeters(planetDistance), 6) * convertEarthRadiusesToMeters(planetRadius) *
-				APPROXIMATE_PLANET_RIGIDITY) /
-				convertEarthMassesToKilograms(planetMass) * Math.pow(convertSunMassesToKilograms(starMass), 2)
-				) > CELESTIAL_TIME_SCALE;
+	/**
+	 * Calculates gravity at a distance from an object. Given the mass of a planet and it's
+	 * radius, gives surface gravity. The correct units are kilograms and meters, but can
+	 * also be used with relative units - for example, giving mass and radius relative to
+	 * Earth, the result is gravity as multiples of Earth's gravity.
+	 * 
+	 * @param mass		object mass (kilograms or relative).
+	 * @param distance	distance from the center of the mass (meters or relative).
+	 * 
+	 * @return	gravity, either as m/s^2 or relative.
+	 */
+	public double calculateGravity(double mass, double distance) {
+		// g = GM / r^2
+		return (GRAVITATIONAL_CONSTANT * mass) / Math.pow(distance, 2);
+	}
+	/**
+	 * Checks if a satellite is (likely to be) tidally locked to it host star/planet (&quot;primary&quot;).
+	 * 
+	 * @param satelliteDistanceInMeters	mean orbital distance from the host. 
+	 * @param satelliteRadiusInMeters	mean radius of the satellite.
+	 * @param satelliteMassInKilograms	mass of the satellite.
+	 * @param primaryMassInKilograms	mass of the host star/planet.
+	 * @return
+	 */
+	public boolean isSatelliteTidallyLocked(double satelliteDistanceInMeters, double satelliteRadiusInMeters,
+			double satelliteMassInKilograms, double primaryMassInKilograms) {
+		BigDecimal satelliteDistance = new BigDecimal(satelliteDistanceInMeters);
+		BigDecimal satelliteMass = new BigDecimal(satelliteMassInKilograms);
+		BigDecimal primaryMass = new BigDecimal(primaryMassInKilograms);
+		BigDecimal approximateTimeToGetTidallyLocked =
+				BigDecimal.valueOf(6).multiply(
+						satelliteDistance.pow(6).multiply(
+								BigDecimal.valueOf(satelliteRadiusInMeters)).multiply(APPROXIMATE_PLANET_RIGIDITY)
+						.divide(
+								satelliteMass.multiply(primaryMass.pow(2)), RoundingMode.HALF_UP
+						)
+				).multiply(TIDAL_LOCKING_FORMULA_TIME_SCALE);
+		return (approximateTimeToGetTidallyLocked.compareTo(CELESTIAL_TIME_SCALE) < 0);
 	}
 	
-	private double convertEarthRadiusesToMeters(int earthRadiuses) {
-		return (earthRadiuses * 100) * EART_RADIUS_IN_METERS;
+	public double convertCentiEarthRadiusesToMeters(int earthRadiuses) {
+		return (earthRadiuses * 100) * EARTH_RADIUS_IN_METERS;
 	}
 	
-	private double convertEarthMassesToKilograms(int earthMasses) {
+	public double convertCentiEarthMassesToKilograms(int earthMasses) {
 		return (earthMasses * 100) * EARTH_MASS_IN_KILOGRAMS;
 	}
 	
-	private double convertSunMassesToKilograms(double sunMasses) {
+	public double convertSunMassesToKilograms(double sunMasses) {
 		return sunMasses * SUN_MASS_IN_KILOGRAMS;
 	}
 	
--- phyraxi-core/src/test/java/phyraxi/utils/PlanetPropertiesCalculatorTest.java
+++ phyraxi-core/src/test/java/phyraxi/utils/PlanetPropertiesCalculatorTest.java
@@ -1,7 +1,10 @@
 package phyraxi.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import phyraxi.domain.LuminosityClass;
@@ -15,14 +18,20 @@ public class PlanetPropertiesCalculatorTest {
 			LuminosityClass.MAIN_SEQUENCE, 1.0, 1.0, 5778);
 	private static final int EARTH_DISTANCE = 100;
 	private static final double EARTH_ALBEDO = 0.306d;
-	private static final double EARTH_EQUILIBRIUM_TEMPERATURE = 254d;
-	private static final double OBSERVED_EARTH_SOLAR_FLUX = 1366d;
-	private static final double SUN_MASS = 1.99e30;
-	private static final double EARTH_MASS = 5.97e24;
+	private static final int EARTH_EQUILIBRIUM_TEMPERATURE = 254;
+	private static final int OBSERVED_EARTH_SOLAR_FLUX = 1366;
+	private static final double SUN_MASS_IN_KILOGRAMS = 1.99e30;
+	private static final double EARTH_MASS_IN_KILOGRAMS = 5.97e24;
+	private static final int EARTH_MEAN_RADIUS_IN_METERS = 6_371_000;
+	private static final double EARTH_GRAVITY = 9.80665;
 	private static final double EARTH_HILL_SPHERE_RADIUS = 1.496e11;
 	private static final int EARTH_DENSITY = 5513;
 	private static final int MOON_DENSITY = 3346;
 	private static final double EARTH_MOON_ROCHE_LIMIT = 1.44d;
+	private static final int MOON_MEAN_ORBITAL_DISTANCE_IN_METERS = 384_399_000;
+	private static final int MOON_MEAN_RADIUS_IN_METERS = 1_737_100;
+	private static final double MOON_MASS_IN_KILOGRAMS = 7.3477e22;
+	private static final double MOON_GRAVITY = 1.6249;
 	
 	private PlanetPropertiesCalculator planetPropertiesCalculator = new PlanetPropertiesCalculator();
 	
@@ -47,7 +56,7 @@ public class PlanetPropertiesCalculatorTest {
 	
 	@Test
 	public void calculateHillSphereRadiusShouldMatchExpected() {
-		double radius = planetPropertiesCalculator.calculateHillSphereRadius(EARTH_DISTANCE, SUN_MASS, EARTH_MASS);
+		double radius = planetPropertiesCalculator.calculateHillSphereRadius(EARTH_DISTANCE, SUN_MASS_IN_KILOGRAMS, EARTH_MASS_IN_KILOGRAMS);
 		assertEquals(EARTH_HILL_SPHERE_RADIUS, radius, 0.1d);
 	}
 	
@@ -55,6 +64,36 @@ public class PlanetPropertiesCalculatorTest {
 	public void calculateRocheLimitShouldMatchExpected() {
 		double limit = planetPropertiesCalculator.calculateRocheLimit(EARTH_DENSITY, MOON_DENSITY);
 		assertEquals(EARTH_MOON_ROCHE_LIMIT, limit, 0.1d);
+	}
+	
+	@Test
+	public void calculateGravityShouldMatchExpectedForEarth() {
+		double gravity = planetPropertiesCalculator.calculateGravity(
+				EARTH_MASS_IN_KILOGRAMS, EARTH_MEAN_RADIUS_IN_METERS);
+		assertEquals(EARTH_GRAVITY, gravity, 0.01d);
+	}
+	
+	@Test
+	public void calculateGravityShouldMatchExpectedForMoon() {
+		double gravity = planetPropertiesCalculator.calculateGravity(
+				MOON_MASS_IN_KILOGRAMS, MOON_MEAN_RADIUS_IN_METERS);
+		assertEquals(MOON_GRAVITY, gravity, 0.01d);
+	}
+	
+	@Test
+	public void moonShouldBeConsideredTidallyLockedToEarth() {
+		assertTrue(planetPropertiesCalculator.isSatelliteTidallyLocked(
+				MOON_MEAN_ORBITAL_DISTANCE_IN_METERS, MOON_MEAN_RADIUS_IN_METERS,
+				MOON_MASS_IN_KILOGRAMS, EARTH_MASS_IN_KILOGRAMS));
+	}
+	
+	@Test
+	@Ignore("Something fishy here - has to be fixed!")
+	public void earthShouldNotBeConsideredTidallyLockedToSun() {
+		assertFalse(planetPropertiesCalculator.isSatelliteTidallyLocked(
+				planetPropertiesCalculator.convertCentiAstronomicalUnitsToMeters(100),
+				planetPropertiesCalculator.convertCentiEarthRadiusesToMeters(100),
+				EARTH_MASS_IN_KILOGRAMS, SUN_MASS_IN_KILOGRAMS));
 	}
 
 }
